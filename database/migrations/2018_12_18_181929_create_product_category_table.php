7fc81b623bad7bac63ee64ca701eacfd060a8a30
Bostjan Janzovnik <bostjan.janzovnik@gmail.com>
2018-12-18 19:36:21 +0100

    Added online store scenario migrations and classes

---
+++ database/migrations/2018_12_18_181929_create_product_category_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_category', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 191);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_category');
+    }
+}
